dec12212cd182049827f22f4ff024d54a2a5712d
Neel Shah <neel.shah@pioneers.io>
2017-07-12 14:55:12 +0200

    Move away from playRunHook, copy SbtJsTask boilerplate to play well with sbt-web

--- project/Build.scala
+++ project/Build.scala
@@ -1,4 +1,6 @@
 
+import java.net.InetSocketAddress
+
 import com.typesafe.sbt.jse.JsEngineImport.JsEngineKeys
 
 import com.typesafe.sbt.digest.Import._
@@ -6,11 +8,11 @@ import com.typesafe.sbt.gzip.Import._
 import com.typesafe.sbt.web.Import._
 import com.typesafe.sbt.web.SbtWeb
 import play.sbt.Play.autoImport._
-import sbt.Keys._
+import play.sbt.PlayRunHook
 import sbt._
-import play.sbt.PlayImport.PlayKeys.playRunHooks
-import com.typesafe.sbt.packager.Keys._
-import Webpack._
+import sbt.Keys._
+
+
 object ApplicationBuild extends Build {
 
   override def settings = super.settings ++ Seq(
@@ -37,7 +39,7 @@ object ApplicationBuild extends Build {
   /** play website */
   lazy val main = Project("main", file("."), settings = commonSettings).
     enablePlugins(play.sbt.PlayScala, SbtWeb).
-    settings(
+    settings(Seq(
       JsEngineKeys.engineType := JsEngineKeys.EngineType.Node,
       libraryDependencies ++= Seq(
         cache, // play cache external module
@@ -45,18 +47,40 @@ object ApplicationBuild extends Build {
         "com.lihaoyi" %% "upickle" % "0.2.8",
         "org.scalatestplus" % "play_2.11" % "1.4.0"
       ),
-      playRunHooks <+= baseDirectory.map(base => Webpack(base)),
-      excludeFilter in Assets := "*.jsx" || "*.js",
-      managedResourceDirectories in Assets ++= (WebKeys.webTarget.value / "webpack" / "main").get,
-      managedResources in Assets <<= managedResources in Assets dependsOn webpack,
-      webpack := Webpack.runDist.value,
-      dist <<= dist dependsOn webpack,
-      stage <<= stage dependsOn webpack,
-      pipelineStages := Seq(digest, gzip)
-
+      pipelineStages := Seq(digest, gzip),
+      WebKeys.exportedMappings in Assets := Seq()
+    ) ++ addJsSourceFileTasks(webpack)
     ).dependsOn(model)
 
-  val webpack = taskKey[Unit]("Run webpack dist")
+  //val webpack = taskKey[Unit]("Run webpack dist")
+  val webpack = taskKey[Seq[File]]("Webpack source file task")
 
+  // from https://github.com/sbt/sbt-js-engine/blob/master/src/main/scala/com/typesafe/sbt/jse/SbtJsTask.scala
+  def addUnscopedJsSourceFileTasks(sourceFileTask: TaskKey[Seq[File]]): Seq[Setting[_]] = {
+    Seq(
+      resourceGenerators <+= sourceFileTask,
+      managedResourceDirectories += (resourceManaged in sourceFileTask).value
+    ) ++ inTask(sourceFileTask)(Seq(
+      sourceDirectories := unmanagedSourceDirectories.value ++ managedSourceDirectories.value,
+      sources := unmanagedSources.value ++ managedSources.value
+    ))
+  }
+
+  def addJsSourceFileTasks(sourceFileTask: TaskKey[Seq[File]]): Seq[Setting[_]] = {
+    Seq(
+      sourceFileTask in Assets := webpackTask.value,
+      resourceManaged in sourceFileTask in Assets := WebKeys.webTarget.value / sourceFileTask.key.label / "main",
+      sourceFileTask := (sourceFileTask in Assets).value
+    ) ++ inConfig(Assets)(addUnscopedJsSourceFileTasks(sourceFileTask))
+  }
+
+  def webpackTask : Def.Initialize[Task[Seq[File]]] = Def.task {
+    val targetDir = WebKeys.webTarget.value / "webpack" / "main"
+    println("running webpack dist")
+    val statusCode = Process("npm run webpack", baseDirectory.value).!
+    if(statusCode > 0) throw new Exception("Webpack failed with exit code : " + statusCode)
+    targetDir.***.get.filter(_.isFile)
+  }
 
 }
+
